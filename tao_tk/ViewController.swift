7c7535adf82f9125eac88c6763743d51aadb0021
ios <ios@techmasters-imac-2.local>
2016-10-07 16:30:58 +0700

    Initial Commit

---
+++ tao_tk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tao_tk
+//
+//  Created by ios on 10/7/16.
+//  Copyright © 2016 ios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
